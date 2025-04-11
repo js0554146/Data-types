01b88ef4c96042325ca8310801fbebe8e0149a78
js0554146 <js0554146@gmail.com>
2025-04-10 20:03:25 -0400

    Data types

---
+++ Data types.py
@@ -0,0 +1,12 @@
+#Storing integers in variables
+a = 5 
+print("type of a: ", type(a)) 
+
+b = 7.5
+print("type of b: ", type(b))
+
+c = True
+print("type of c: ", type(c))
+
+d = "Cruz"
+print("type of c: ", type(d))
